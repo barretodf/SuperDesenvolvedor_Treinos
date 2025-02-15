341e8469723b50e01408b9c73f73c864f2e3a4ae
Edmilson Barreto <edsb1981@gmail.com>
2025-02-15 02:18:19 -0300

    Deletado arquivos duplicados

--- lacoRepeticaoUntil.rb
+++
@@ -1,7 +0,0 @@
-print "Digite um número entre 1 e 10: "
-contador = gets.chomp.to_i
-
-until contador > 10
-    puts contador
-    contador += 1
-end
--- somarDoisNumeros.rb
+++
@@ -1,8 +0,0 @@
-print "Dgite um número: "
-num1 = gets.chomp.to_i
-
-print "Digite outro número: "
-num2 = gets.chomp.to_i
-
-soma = num1 + num2
-puts "A soma de: #{num1} + #{num2} = #{soma}"
