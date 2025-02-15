481a969d9fe7d7e4ed227bfb19ac5fd9a26ed003
Edmilson Barreto <edsb1981@gmail.com>
2025-02-14 23:43:25 -0300

    Primeiro commit: estrutura inicial do projeto

---
+++ lib/StickFlow_music_creator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "StickFlow_music_creator/version"
+
+module StickFlowMusicCreator
+  class Error < StandardError; end
+  # Your code goes here...
+end
