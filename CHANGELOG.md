9116e78c86fc29af75026262c8dd9011b9b5df83
Edmilson Barreto <edsb1981@gmail.com>
2025-02-15 00:14:15 -0300

    Removendo arquivos desnecessários do GitHub

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-## [Unreleased]
-
-## [0.1.0] - 2025-02-14
-
-- Initial release
--- lib/StickFlow_music_creator.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "StickFlow_music_creator/version"
-
-module StickFlowMusicCreator
-  class Error < StandardError; end
-  # Your code goes here...
-end
