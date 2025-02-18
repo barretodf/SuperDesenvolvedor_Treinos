a4a9b9eb5e84acd8a9d885f98ebb0146d0c4aa8d
Edmilson Barreto <edsb1981@gmail.com>
2025-02-18 20:55:06 -0300

    Adicionado Algoritmo de combinação -  produto cartesiano

--- lib/nomeshow.rb
+++ lib/nomeshow.rb
@@ -1,15 +1,33 @@
-# module NomeShow
-#   def self.gerar_nome(frase)
-#     palavras = frase.split
-#     palavras.shuffle.join(" ")
-#   end
-# end
-require 'debug'
+# lib/nomeshow.rb
 
 module NomeShow
-  def self.gerar_nome(frase)
-    debuger
-    palavras = frase.split
-    palavras.shuffle.join(" ")
+  # Listas de palavras para a geração de títulos
+  ADJETIVOS = ["incrível", "surpreendente", "fantástico", "épico", "impressionante"]
+  SUBSTANTIVOS = ["viagem", "aventura", "truque", "desafio", "dicas"]
+  FRASES = ["não vai acreditar", "o segredo revelado", "como fazer", "truques secretos"]
+
+  # Método Template-Driven: Gera um título aleatório combinando um item de cada lista
+  def self.gerar_titulo
+    adjetivo = ADJETIVOS.sample         # Escolhe um adjetivo aleatório
+    substantivo = SUBSTANTIVOS.sample     # Escolhe um substantivo aleatório
+    frase = FRASES.sample                 # Escolhe uma frase aleatória
+
+    # Retorna o título formatado
+    "#{adjetivo.capitalize} #{substantivo} - #{frase}"
+  end
+
+  # Algoritmo de Combinação (Produto Cartesiano):
+  # Gera todas as combinações possíveis entre os elementos das listas.
+  def self.titulos_cartesianos
+    # Cria o produto cartesiano dos arrays e formata cada combinação como um título
+    ADJETIVOS.product(SUBSTANTIVOS, FRASES).map do |comb|
+      "#{comb[0].capitalize} #{comb[1]} - #{comb[2]}"
+    end
+  end
+
+  # Seleciona aleatoriamente um título dentre todas as combinações possíveis.
+  def self.gerar_titulo_cartesiano
+    titulos = titulos_cartesianos   # Gera todas as combinações
+    titulos.sample                  # Escolhe uma aleatoriamente
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,10 @@
-# frozen_string_literal: true
+# spec/spec_helper.rb
 
-require "nomeshow"
+# Adiciona a pasta 'lib' ao $LOAD_PATH para que o Ruby consiga encontrar o arquivo 'nomeshow.rb'
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
+require "nomeshow"  # Isso procura por 'lib/nomeshow.rb'
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
+  # Configurações adicionais do RSpec, se necessário
 end
