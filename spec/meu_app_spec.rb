2721ec9197c580105bc92c6e47245ea2bf729182
Edmilson Barreto <edsb1981@gmail.com>
2025-02-19 15:42:17 -0300

    Removidos arquivos abertos_fora de pasta

--- spec/meu_app_spec.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../lib/StickFlow_Music/melodia'
-
-RSpec.describe Melodia do
-  it 'retorna a mensagem correta ao tocar' do
-    expect(Melodia.tocar).to eq("Tocando melodia...")
-  end
-end
